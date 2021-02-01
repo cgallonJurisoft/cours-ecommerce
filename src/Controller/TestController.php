635d14ff424bb95cd4c7a980a42bc08f62b1d4b5
cgallonJurisoft <c.gallon@jurisoft.fr>
2021-02-01 17:19:47 +0100

    HttpFoundation : Requête et Réponse HTTP !

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class TestController {
   public function index() {
     dd("C OKé");
   } 
-  public function test() {
-    dd("C test");
+  public function test(Request $request) {
+    //$request = Request::createFromGlobals();
+    $age = $request->query->get('age', 0);
+    return new Response("T'as $age ans");
   } 
 }
