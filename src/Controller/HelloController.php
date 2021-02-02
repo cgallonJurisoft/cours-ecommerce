339726e1d5e3b9d787491e994d18cdb07d1863f9
cgallonJurisoft <c.gallon@jurisoft.fr>
2021-02-02 10:49:53 +0100

    Introduction au container de services de Symfony

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController {
+
+  protected $logger;
+
+  public function __construct(LoggerInterface $logger) {
+    $this->logger = $logger;
+  }
 
   /**
    * @Route("/hello/{prenom<\p{L}+>?World}", 
    * name="hello")
    */
   public function hello($prenom) {
+    $this->logger->error("Mon message de log");
     return new Response("Hello $prenom!");
   } 
 }
