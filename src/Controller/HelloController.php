612cb9b04830e6541ebe3a20fe0fdca542fe7113
cgallonJurisoft <c.gallon@jurisoft.fr>
2021-02-02 11:30:41 +0100

    Créez vos propres services !

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,6 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloController {
 
   protected $logger;
+  protected $calculator;
 
   public function __construct(LoggerInterface $logger) {
     $this->logger = $logger;
@@ -18,8 +20,12 @@ class HelloController {
    * @Route("/hello/{prenom<\p{L}+>?World}", 
    * name="hello")
    */
-  public function hello($prenom) {
+  public function hello($prenom, Calculator $calculator) {
     $this->logger->error("Mon message de log");
+    $tva = $calculator->calcul(100);
+
+    dump($tva);
+
     return new Response("Hello $prenom!");
   } 
 }
