e57a164c9448852b5b0d7661b9c4f50b381b0444
cgallonJurisoft <c.gallon@jurisoft.fr>
2021-02-16 12:46:21 +0100

    Correction #01 : Créez un service "Detector"

--- src/Taxes/Detector.php
+++ src/Taxes/Detector.php
@@ -4,8 +4,8 @@ namespace App\Taxes;
 
 class Detector {
 
-  public function detect(int $nb) : bool {
-    return $nb > 100;
+  public function detect(float $prix) : bool {
+    return $prix > 100;
   }
 
 }
